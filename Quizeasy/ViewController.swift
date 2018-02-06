ab832ca2008041a3e6403f44cb174fe83f83f24a
mariyaeggs <mariyawilson@live.com>
2018-02-06 14:11:56 -0800

    Setup project in MVC structure with designs.

---
+++ Quizeasy/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quizeasy
+//
+//  Created by Mariya Eggensperger on 02/06/2018.
+//  Copyright (c) 2018 Mariya Eggensperger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Instance variables
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func answerTapped(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
